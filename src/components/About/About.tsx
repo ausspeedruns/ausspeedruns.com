a40c0d64b007eebb10e66e8f15e78691d37e649d
Steph Westgarth <stephwestgarth@gmail.com>
2021-06-08 23:41:09 +1000

    Added About component

---
+++ src/components/About/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h3>About AusSpeedruns</h3>
+      <p>
+        Also known as Australian Speedrunners, AusSpeedruns is a not-for-profit
+        organisation that brings together the best speedrunners in Australia to
+        raise money and awareness for charity at events across Australia.
+      </p>
+    </div>
+  );
+};
+
+export default About;

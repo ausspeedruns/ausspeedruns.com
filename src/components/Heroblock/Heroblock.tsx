3276d92212cd6553d4b1605bb8d23d865e1616cf
Steph Westgarth <stephwestgarth@gmail.com>
2021-06-08 23:40:28 +1000

    Updated Hero block

--- src/components/Heroblock/Heroblock.tsx
+++ src/components/Heroblock/Heroblock.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import './Heroblock.scss'
-import '../../styles/buttons.scss'
-import TwitchEmbed from './TwitchEmbed'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./Heroblock.scss";
+import "../../styles/buttons.scss";
+// import TwitchEmbed from './TwitchEmbed'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import Countdown, { zeroPad, CountdownRenderProps } from "react-countdown";
 
 const Heroblock = () => {
+  const countdownRender = ({
+    days,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+    completed,
+  }: CountdownRenderProps) => {
+    if (completed) return <span></span>;
+    else if (days > 0)
+      return (
+        <span>
+          <span className="sr-only">
+            {days} days, {hours} hours, {minutes} minutes and {seconds} seconds
+            remaining
+          </span>
+          <span aria-hidden>
+            {zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:
+            {zeroPad(seconds)}
+          </span>
+        </span>
+      );
+    else if (hours > 0)
+      return (
+        <span>
+          <span className="sr-only">
+            {hours} hours, {minutes} minutes and {seconds} seconds remaining
+          </span>
+          <span aria-hidden>
+            {zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+          </span>
+        </span>
+      );
+    else
+      return (
+        <span>
+          <span className="sr-only">
+            {minutes} minutes, {seconds} seconds and {milliseconds} milliseconds
+            remaining
+          </span>
+          <span aria-hidden>
+            {zeroPad(minutes)}:{zeroPad(seconds)}:{zeroPad(milliseconds)}
+          </span>
+        </span>
+      );
+  };
+
   return (
     <div className="heroblock">
       <div className="content">
-        <TwitchEmbed channel="ausspeedruns" parent={window.location.hostname}/>
+        {/* <TwitchEmbed channel="ausspeedruns" parent={window.location.hostname}/> */}
         <div className="ctaBlock">
           <h1>Australian Speedrun Marathon 2021</h1>
           <h2>July 13th - 18th</h2>
-          <p>Australian Speedrunners come together to raise money for Beyond Blue at ASM2021! Hosted at the Oaks Adelaide Embassy Suites, Australia.</p>
-          <a className="button primary" href="#">Donate <FontAwesomeIcon icon={ faChevronRight } /></a>
+          <h3>
+            <Countdown
+              date={Date.parse("13 July 2021 11:00:00 GMT+1100")}
+              renderer={countdownRender}
+              zeroPadTime={2}
+            />
+          </h3>
+          <p>
+            Australian Speedrunners come together to raise money for Beyond Blue
+            at ASM2021! Hosted at the Oaks Adelaide Embassy Suites, Australia.
+          </p>
+          {/* <a className="button primary" href="//fundraise.beyondblue.org.au/asm2021">Donate <FontAwesomeIcon icon={ faChevronRight } /></a> */}
+          <a className="button secondary" href="#participate">
+            Get involved <FontAwesomeIcon icon={faChevronRight} />
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Heroblock;
--- src/components/Heroblock/TwitchEmbed.tsx
+++ src/components/Heroblock/TwitchEmbed.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import './TwitchEmbed.scss'
+import React from "react";
+import "./TwitchEmbed.scss";
 
 type TwitchProps = {
-  channel: string,
-  parent: string,
-  muted?: boolean
-}
+  channel: string;
+  parent: string;
+  muted?: boolean;
+};
 
-const TwitchEmbed = ({ channel, parent, muted = true } : TwitchProps) => {
-
+const TwitchEmbed = ({ channel, parent, muted = true }: TwitchProps) => {
   return (
     <div className="twitchEmbed">
       <iframe
+        title="Ausspeedruns twitch player"
         src={`https://player.twitch.tv/?channel=${channel}&parent=${parent}&muted=${muted}`}
         allowFullScreen
-        >
-      </iframe>
+      ></iframe>
     </div>
   );
-}
+};
 
 export default TwitchEmbed;

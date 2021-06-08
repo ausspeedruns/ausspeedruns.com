636127c26b903c6c27b71118ad204fc8a320afeb
Steph Westgarth <stephwestgarth@gmail.com>
2021-06-08 23:40:48 +1000

    Updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,49 +1,62 @@
-import React from 'react';
-import './Navbar.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react";
+import "./Navbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faTwitch, faTwitter, faDiscord, faYoutube
-} from '@fortawesome/free-brands-svg-icons'
+  faTwitch,
+  faTwitter,
+  faDiscord,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="title">
-        <a href="/"><span className="logo"></span><span>AusSpeedruns</span></a>
+      <div className="content">
+        <div className="title">
+          <a href="/">
+            <span className="logo"></span>
+            <span>AusSpeedruns</span>
+          </a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="//schedule.ausspeedruns.com">Schedule</a>
+            </li>
+            {/* <li><a href="//incentives.ausspeedruns.com">Incentives</a></li> */}
+            <li>
+              <a href="#participate">Get Involved</a>
+            </li>
+            <li className="social">
+              <a href="//www.twitch.tv/ausspeedruns">
+                <FontAwesomeIcon icon={faTwitch} />
+                <span className="text sr-only">Twitch</span>
+              </a>
+            </li>
+            <li className="social">
+              <a href="//twitter.com/ausspeedruns">
+                <FontAwesomeIcon icon={faTwitter} />
+                <span className="text sr-only">Twitter</span>
+              </a>
+            </li>
+            <li className="social">
+              <a href="//youtube.com/ausspeedruns">
+                <FontAwesomeIcon icon={faYoutube} />
+                <span className="text sr-only">Youtube</span>
+              </a>
+            </li>
+            <li className="social">
+              <a href="//discord.ausspeedruns.com/">
+                <FontAwesomeIcon icon={faDiscord} />
+                <span className="text sr-only">Discord</span>
+              </a>
+            </li>
+            {/* <li><a className="button secondary" href="//fundraise.beyondblue.org.au/asm2021">Donate</a></li> */}
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li><a href="//schedule.ausspeedruns.com">Schedule</a></li>
-          <li><a href="//incentives.ausspeedruns.com">Incentives</a></li>
-          <li className="social">
-            <a href="//www.twitch.tv/ausspeedruns">
-              <FontAwesomeIcon icon={faTwitch} />
-              <span className="text">Twitch</span>
-            </a>
-          </li>
-          <li className="social">
-            <a href="//twitter.com/ausspeedruns">
-              <FontAwesomeIcon icon={faTwitter} />
-              <span className="text">Twitter</span>
-            </a>
-          </li>
-          <li className="social">
-            <a href="http://youtube.com/ausspeedruns">
-              <FontAwesomeIcon icon={faYoutube} />
-              <span className="text">Youtube</span>
-            </a>
-          </li>
-          <li className="social">
-            <a href="http://discord.ausspeedruns.com/">
-              <FontAwesomeIcon icon={faDiscord} />
-              <span className="text">Discord</span>
-            </a>
-          </li>
-          <li><a className="button secondary" href="//donate.ausspeedruns.com">Donate</a></li>
-        </ul>
-      </nav>
     </div>
   );
-}
+};
 
 export default Navbar;

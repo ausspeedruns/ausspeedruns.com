31a386b1b835fcc78c3c251b42c0fedc464b3281
Steph Westgarth <stephwestgarth@gmail.com>
2021-06-08 23:40:14 +1000

    Updated EventDetails component

--- src/components/EventDetails/EventDetails.tsx
+++ src/components/EventDetails/EventDetails.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import './EventDetails.scss'
+import React from "react";
+import "./EventDetails.scss";
+import beyondBlue from "../../styles/img/bblue-blue.png";
+import oaksHotels from "../../styles/img/oaks-hotels.png";
 
 const EventDetails = () => {
   return (
     <div className="eventDetails">
       <div className="content">
         <div className="charities">
-          <h3>Proudly supporting</h3>
           <div className="charity">
-            Beyond Blue
+            <img src={beyondBlue} alt="Proudly supporting Beyond Blue" />
           </div>
         </div>
         <div className="sponsors">
           <h3>Event sponsors</h3>
           <div className="sponsor">
-            Oaks Hotels
+            <img src={oaksHotels} alt="Proudly supporting Beyond Blue" />
           </div>
-          <div className="sponsor">
-            NVIDIA
-          </div>
-          <p>Sponsors and donations make our events possible. If you would like to sponsor us for a future event, get in touch!</p>
-          </div>
+          <p>
+            Sponsors and donations make our events possible. If you would like
+            to sponsor us for a future event, get in touch!
+          </p>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default EventDetails;

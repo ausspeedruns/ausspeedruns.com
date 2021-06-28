a239a8b54aeb89f5bccedbcccef9198071727f4a
Steph Westgarth <stephwestgarth@gmail.com>
2021-06-28 12:52:43 +1000

    Nav improved to behave better on mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,6 +10,15 @@ import {
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+  const [isMobile, setIsMobile] = useState<Boolean>(true);
+  
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setIsMobile(window.innerWidth < 768)
+    })
+  })
+
   return (
     <div className="navbar">
       <div className="content">
@@ -19,7 +28,18 @@ const Navbar = () => {
             <h1>AusSpeedruns</h1>
           </a>
         </div>
-        <nav id="main-menu" className="main-menu" aria-label="Main menu">
+        <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)} aria-expanded={isOpen.valueOf()}>
+          { !isOpen ? (
+          <FontAwesomeIcon icon={faBars} />
+          ) : (
+            <FontAwesomeIcon icon={faTimes} />
+          )}
+          <span className="sr-only">Menu</span>
+        </button>
+        { isMobile ? (<div className="break"></div>
+        ) : ''}
+        
+        <nav className={`main-menu ${isOpen ? 'menu-open' : 'menu-closed'}`} aria-label="Main menu">
           <ul>
             <li>
               <a href="//schedule.ausspeedruns.com">Schedule</a>
@@ -31,51 +51,30 @@ const Navbar = () => {
             <li className="social">
               <a href="//www.twitch.tv/ausspeedruns">
                 <FontAwesomeIcon icon={faTwitch} />
-                <span className="text sr-only">Twitch</span>
+                <span className={`text ${isMobile ? '' : 'sr-only'}`}>Follow us on Twitch</span>
               </a>
             </li>
             <li className="social">
               <a href="//twitter.com/ausspeedruns">
                 <FontAwesomeIcon icon={faTwitter} />
-                <span className="text sr-only">Twitter</span>
+                <span className={`text ${isMobile ? '' : 'sr-only'}`}>Follow us on Twitter</span>
               </a>
             </li>
             <li className="social">
               <a href="//youtube.com/ausspeedruns">
                 <FontAwesomeIcon icon={faYoutube} />
-                <span className="text sr-only">Youtube</span>
+                <span className={`text ${isMobile ? '' : 'sr-only'}`}>Subscribe to AusSpeedruns on Youtube</span>
               </a>
             </li>
             <li className="social">
               <a href="//discord.ausspeedruns.com/">
                 <FontAwesomeIcon icon={faDiscord} />
-                <span className="text sr-only">Discord</span>
+                <span className={`text ${isMobile ? '' : 'sr-only'}`}>Join us on Discord</span>
               </a>
             </li>
             {/* <li><a className="button secondary" href="//fundraise.beyondblue.org.au/asm2021">Donate</a></li> */}
           </ul>
         </nav>
-        <a href="#main-menu"
-          id="main-menu-toggle"
-          className="menu-toggle"
-          aria-label="Open main menu">
-          <span className="sr-only">Open main menu</span>
-          <FontAwesomeIcon icon={faBars} aria-hidden />
-        </a>
-
-        <a href="#main-menu-toggle"
-          id="main-menu-close"
-          className="menu-close"
-          aria-label="Close main menu">
-          <span className="sr-only">Close main menu</span>
-          <FontAwesomeIcon icon={faTimes} aria-hidden />
-        </a>
-
-        <a href="#main-menu-toggle"
-          className="backdrop"
-          tabIndex={-1}
-          aria-hidden="true" hidden>
-        </a>
       </div>
     </div>
   );

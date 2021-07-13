ab71f3601cdf726066a638d4b1dca28db837ee7b
Steph Westgarth <stephwestgarth@gmail.com>
2021-07-13 11:42:46 +1000

    Added global file for standard terms/links

---
+++ src/globals.ts
@@ -0,0 +1,24 @@
+export const globals = {
+  events: {
+    current: {
+      fullName: 'Australian Speedrun Marathon 2021',
+      preferredName: 'AusSpeedrun Marathon 2021',
+      shortName: 'ASM2021',
+      date: "14 July 2021 11:00:00 GMT+1000",
+    },
+    next: {
+      fullName: 'PAX 2021',
+      preferredName: 'PAX 2021',
+      shortName: 'PAX2021',
+      date: "08 October 2021 10:00:00 GMT+1000",
+    },
+  },
+  donateLink: 'http://donate.ausspeedruns.com/',
+  scheduleLink: 'http://schedule.ausspeedruns.com/',
+  socialLinks: {
+    discord: 'http://discord.ausspeedruns.com/',
+    twitter: '//twitter.com/ausspeedruns',
+    youtube: '//youtube.com/ausspeedruns',
+    twitch: '//twitch.tv/ausspeedruns'
+  }
+}

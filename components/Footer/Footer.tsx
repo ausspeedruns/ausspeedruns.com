16e8d98aad1112c281a7e3b86379f51dbf86d178
Ewan Lyon <clubwhomc@gmail.com>
2022-02-15 20:59:15 +1100

    Add policies page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,80 +1,84 @@
 import {
-  faTwitch,
-  faTwitter,
-  faYoutube,
-  faDiscord,
-  faInstagram,
-  // faFacebook,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import styles from "./Footer.module.scss";
-import { globals } from "../../pages/globals";
+	faTwitch,
+	faTwitter,
+	faYoutube,
+	faDiscord,
+	faInstagram,
+	// faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styles from './Footer.module.scss';
+import { globals } from '../../pages/globals';
+import Link from 'next/link';
 
 interface FooterProps {
-  className?: string;
+	className?: string;
+	hideAcknowledgement?: boolean;
 }
 
 const Footer = (props: FooterProps) => {
-  return (
-    <div className={props.className}>
-      <div className={styles.acknowledgement}>
-        <div className="content">
-          <p>
-            In the spirit of reconciliation, Australian Speedrunners
-            acknowledges the Traditional Custodians of country throughout
-            Australia and their connections to land, sea and community. We pay
-            our respect to their elders past and present and extend that respect
-            to all Aboriginal and Torres Strait Islander peoples today.
-          </p>
-        </div>
-      </div>
-      <div className={styles.footer}>
-        <div className="content">
-          <ul>
-            <li className="social">
-              <a href={globals.socialLinks.twitch}>
-                <FontAwesomeIcon icon={faTwitch} />
-                <span className={styles.text}>Follow us on Twitch</span>
-              </a>
-            </li>
-            {/* <li className="social">
+	return (
+		<div className={props.className}>
+			{!props.hideAcknowledgement && (
+				<div className={styles.acknowledgement}>
+					<div className="content">
+						<p>
+							In the spirit of reconciliation, Australian Speedrunners acknowledges the Traditional Custodians of
+							country throughout Australia and their connections to land, sea and community. We pay our respect to their
+							elders past and present and extend that respect to all Aboriginal and Torres Strait Islander peoples
+							today.
+						</p>
+					</div>
+				</div>
+			)}
+			<div className={styles.footer}>
+        <div className={`content ${styles.content}`}>
+					<ul>
+						<li className="social">
+							<a href={globals.socialLinks.twitch}>
+								<FontAwesomeIcon icon={faTwitch} />
+								<span className={styles.text}>Follow us on Twitch</span>
+							</a>
+						</li>
+						{/* <li className="social">
               <a href={globals.socialLinks.facebook}>
                 <FontAwesomeIcon icon={faFacebook} />
                 <span className={styles.text}>Like us on Facebook</span>
               </a>
             </li> */}
-            <li className="social">
-              <a href={globals.socialLinks.twitter}>
-                <FontAwesomeIcon icon={faTwitter} />
-                <span className={styles.text}>Follow us on Twitter</span>
-              </a>
-            </li>
-            <li className="social">
-              <a href={globals.socialLinks.youtube}>
-                <FontAwesomeIcon icon={faYoutube} />
-                <span className={styles.text}>
-                  Subscribe on Youtube
-                </span>
-              </a>
-            </li>
-            <li className="social">
-              <a href={globals.socialLinks.discord}>
-                <FontAwesomeIcon icon={faDiscord} />
-                <span className={styles.text}>Join us on Discord</span>
-              </a>
-            </li>
-            <li className="social">
-              <a href={globals.socialLinks.instagram}>
-                <FontAwesomeIcon icon={faInstagram} />
-                <span className={styles.text}>Follow us on Instagram</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+						<li className="social">
+							<a href={globals.socialLinks.twitter}>
+								<FontAwesomeIcon icon={faTwitter} />
+								<span className={styles.text}>Follow us on Twitter</span>
+							</a>
+						</li>
+						<li className="social">
+							<a href={globals.socialLinks.youtube}>
+								<FontAwesomeIcon icon={faYoutube} />
+								<span className={styles.text}>Subscribe on Youtube</span>
+							</a>
+						</li>
+						<li className="social">
+							<a href={globals.socialLinks.discord}>
+								<FontAwesomeIcon icon={faDiscord} />
+								<span className={styles.text}>Join us on Discord</span>
+							</a>
+						</li>
+						<li className="social">
+							<a href={globals.socialLinks.instagram}>
+								<FontAwesomeIcon icon={faInstagram} />
+								<span className={styles.text}>Follow us on Instagram</span>
+							</a>
+						</li>
+					</ul>
+					<div className={styles.miscLinks}>
+						<Link href="/policies">Policies</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
---
+++ pages/policies.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Head from 'next/head';
+
+import styles from '../styles/Policies.module.scss';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+import Link from 'next/link';
+
+const PoliciesPage = () => {
+	return (
+		<div className={`app ${styles.app}`}>
+			<Head>
+				<title>Policies - AusSpeedruns</title>
+			</Head>
+			<header className="App-header">
+				<Navbar />
+			</header>
+			<main className={`content ${styles.content}`}>
+				<h2>Policies</h2>
+				<p>
+					<a target='_blank' href="https://docs.google.com/document/d/1xsmquXa8QzhzlZY59W5iTncvxhrPV2h1TZo8UNFZHUM/edit?usp=sharing">
+						External link to Google Doc containing all policies.
+					</a>
+				</p>
+			</main>
+			<Footer className={styles.footer} />
+		</div>
+	);
+};
+
+export default PoliciesPage;

15fa63623960caac84818c928104b79195dda0df
Ewan Lyon <clubwhomc@gmail.com>
2022-03-07 22:45:44 +1100

    Remove urql devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 // import '../styles/globals.css'
 import '../styles/global.scss';
 import '../styles/App.scss';
-import { createClient, Provider, defaultExchanges } from 'urql';
-import { devtoolsExchange } from '@urql/devtools';
+import { createClient, Provider } from 'urql';
 
 import { AuthProvider } from '../components/auth';
 
 export const client = createClient({
 	url: typeof window === undefined ? 'http://localhost:8000/api/graphql' : '/api/graphql',
-	exchanges: [devtoolsExchange, ...defaultExchanges],
 });
 
 function MyApp({ Component, pageProps }) {

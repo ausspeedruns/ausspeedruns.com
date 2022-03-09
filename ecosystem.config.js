b344fc40308f6315e2e8539e67adc580b4ccebe1
Ewan Lyon <clubwhomc@gmail.com>
2022-03-09 23:27:24 +1100

    Fix start command

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		{
 			name: "ausruns-website",
 			script: './node_modules/next/dist/bin/next',
-			args: 'start -p' + (process.env.PORT || 3000), 
+			args: 'start -p ' + (process.env.PORT || 3000), 
             watch: false,
             autorestart: true,
 		}

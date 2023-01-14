9359cb8eeca127089f264e7db857811a0afd9c31
Ewan Lyon <websites@ewanlyon.com>
2023-01-14 22:43:04 +1100

    fix keystone

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,34 +1,33 @@
-const path = require('path');
-require('dotenv').config();
+const withNx = require("@nrwl/next/plugins/with-nx");
+const path = require("path");
+require("dotenv").config();
 
-module.exports = {
+module.exports = withNx({
 	reactStrictMode: true,
 	sassOptions: {
-		includePaths: [path.join(__dirname, 'styles')],
+		includePaths: [path.join(__dirname, "styles")],
 	},
 	async rewrites() {
 		return [
 			{
-				source: '/api/graphql',
-				// destination: `http://localhost:8000/api/graphql`,
+				source: "/api/graphql",
 				destination:
-					process.env.NODE_ENV === 'production'
-						? `https://keystone.ausspeedruns.com/api/graphql`
-						: `http://localhost:8000/api/graphql`,
+					process.env.NODE_ENV === "production"
+						? "https://keystone.ausspeedruns.com/api/graphql"
+						: "http://localhost:8000/api/graphql",
 			},
 		];
 	},
 	images: {
 		domains: [
-			'localhost',
-			'127.0.0.1',
-			'ausspeedruns.com',
-			'beta.ausspeedruns.com',
-			'ausrunsstoragebeta.blob.core.windows.net',
-			'ausrunsstoragebeta.blob.core.windows.net',
-			'ausrunsstorage.blob.core.windows.net',
-			'ausspeedruns.sharepoint.com',
+			"localhost",
+			"127.0.0.1",
+			"ausspeedruns.com",
+			"beta.ausspeedruns.com",
+			"ausrunsstoragebeta.blob.core.windows.net",
+			"ausrunsstoragebeta.blob.core.windows.net",
+			"ausrunsstorage.blob.core.windows.net",
+			"ausspeedruns.sharepoint.com",
 		],
 	},
-	output: 'standalone',
-};
+});

77747cb937c993c7a01c84f54234a47439f43395
Ewan Lyon <websites@ewanlyon.com>
2023-01-16 01:21:31 +1100

    update copy

--- apps/nextjs/components/Heroblock/Heroblock.tsx
+++ apps/nextjs/components/Heroblock/Heroblock.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import styles from './Heroblock.module.scss';
-import Image from 'next/image';
-import { faChevronRight, faTicket } from '@fortawesome/free-solid-svg-icons';
-import Button from '../Button/Button';
-import { AusSpeedrunsEvent } from '../../types/types';
+import React, { useEffect, useState } from "react";
+import styles from "./Heroblock.module.scss";
+import Image from "next/image";
+import { faChevronRight, faTicket } from "@fortawesome/free-solid-svg-icons";
+import Button from "../Button/Button";
+import { AusSpeedrunsEvent } from "../../types/types";
 
 type HeroblockProps = {
 	event: AusSpeedrunsEvent;
 };
 
 function zeroPad(num: number): string {
-	return num.toString().padStart(2, '0');
+	return num.toString().padStart(2, "0");
 }
 
 const eventDate = Date.UTC(2023, 2, 11, 0, 0, 0, 0);
@@ -29,10 +29,12 @@ function countdownRender(currentTime: number) {
 		return (
 			<span>
 				<span className="sr-only">
-					{days} days, {hours} hours, {minutes} minutes and {seconds} seconds remaining
+					{days} days, {hours} hours, {minutes} minutes and {seconds}{" "}
+					seconds remaining
 				</span>
 				<span aria-hidden>
-					{zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+					{zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:
+					{zeroPad(seconds)}
 				</span>
 			</span>
 		);
@@ -42,7 +44,8 @@ function countdownRender(currentTime: number) {
 		return (
 			<span>
 				<span className="sr-only">
-					{hours} hours, {minutes} minutes and {seconds} seconds remaining
+					{hours} hours, {minutes} minutes and {seconds} seconds
+					remaining
 				</span>
 				<span aria-hidden>
 					{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
@@ -81,33 +84,47 @@ const Heroblock = ({ event }: HeroblockProps) => {
 	return (
 		<section
 			className={styles.heroblock}
-			style={{ backgroundImage: `url("${require(`../../styles/img/${event.heroImage}`).default.src}")` }}
-		>
+			style={{
+				backgroundImage: `url("${
+					require(`../../styles/img/${event.heroImage}`).default.src
+				}")`,
+			}}>
 			<div className={`${styles.content} content`}>
 				<div className={styles.ctaBlock}>
 					<h1>{event.preferredName}</h1>
 					<h2>{event.dates}</h2>
-					<h3 className={[styles.countdown, styles.monospaced].join(' ')}>
+					<h3
+						className={[styles.countdown, styles.monospaced].join(
+							" ",
+						)}>
 						{/* <Countdown date={eventDate} renderer={countdownRender} zeroPadTime={2} /> */}
 						{countdownElement}
 					</h3>
 					<br />
-					<p>We will be at The Game Expo! Game submissions for ASGX2023 are OPEN!</p>
+					<p>
+						We will be at The Game Expo! Submissions have CLOSED.
+						The schedule will be released shortly!
+					</p>
 					<Button
 						actionText={event.preferredName}
-						link={'/ASGX2023'}
+						link={"/ASGX2023"}
 						iconRight={faChevronRight}
-						colorScheme={'secondary'}
+						colorScheme={"secondary"}
 					/>
-					<Button actionText="Purchase Tickets" link={event.website} iconRight={faTicket} colorScheme={'secondary'} />
+					<Button
+						actionText="Purchase Tickets"
+						link={event.website}
+						iconRight={faTicket}
+						colorScheme={"secondary"}
+					/>
 				</div>
 				<div className={styles.logoBlock}>
 					<Image
 						src={require(`../../styles/img/${event.logo}`).default}
 						alt="Event Logo"
 						style={{
-							maxWidth: '100%',
-							height: 'auto',
+							maxWidth: "100%",
+							height: "auto",
 						}}
 					/>
 				</div>

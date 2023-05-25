a6498f96c87c3e4c916dd796c5a763efe10879d0
el-ausspeedruns <128760232+el-ausspeedruns@users.noreply.github.com>
2023-05-25 22:21:12 +0930

    Update sign-up.ts
    Adds support for GET, finds barcode for purpose of setSignedUp

--- apps/nextjs/pages/api/asmm/sign-up.ts
+++ apps/nextjs/pages/api/asmm/sign-up.ts
@@ -1,22 +1,49 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import sql from 'mssql';
 import { connect } from './db';
+import { z } from "zod";
+
+const bodyData = z.object ({
+	username: z.string(),
+	barcode: z.string(),
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method !== "POST") {
-		return res.status(405).setHeader("Allow", "POST");
-	}
-
-	try {
-		const body = JSON.parse(req.body);
+	
+	try {	
 		await connect(sql);
 
-		const data = await sql.query`
-		INSERT INTO [dbo].[Participants]
-		VALUES (${body.username},${body.ticketID ?? "NULL"})`;
+		//Try find barcode for setSignedUp
+		if (req.method === "GET") {
 
-		return res.status(200).json({ success: true });
+			const data = await sql.query`
+			SELECT Barcode
+			FROM [dbo].[Participants]
+			WHERE Username = ${req.query.username}
+			`;
+
+			console.log(data)
+
+			if (!data.recordset[0]) return res.status(200).json({ barcode: NULL });
+			
+			return res.status(200).json({ barcode: data.recordset[0].Barcode });
+
+		}
+
+		if (req.method === "POST" {
+			
+			
+
+			const data = await sql.query`
+			INSERT INTO [dbo].[Participants]
+			VALUES (${body.username},${body.ticketID ?? "NULL"})`;
+
+			return res.status(200).json({ success: true });
+		}
+
 	} catch (error) {
 		return res.status(500).json({ error: error });
 	}
+
+	return res.status(405).setHeader("Allow", ["POST", "GET"]);
 }

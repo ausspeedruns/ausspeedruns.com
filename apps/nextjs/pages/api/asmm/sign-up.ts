4b07178d622ea81c9a274c00ad25ebeaf3c746d8
el-ausspeedruns <128760232+el-ausspeedruns@users.noreply.github.com>
2023-05-26 20:11:24 +0930

    Update sign-up.ts
    DAMN BRACKET!

--- apps/nextjs/pages/api/asmm/sign-up.ts
+++ apps/nextjs/pages/api/asmm/sign-up.ts
@@ -24,7 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 		}
 
-		if (req.method === "POST" {
+		if (req.method === "POST") {
 			
 		    	const body = JSON.parse(req.body);
 		    

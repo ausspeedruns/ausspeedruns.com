bc9690f822bb35b3dd3e4f8f542635ad72c2b7e8
Ewan Lyon <websites@ewanlyon.com>
2024-01-22 22:49:14 +1100

    update order of navbar

--- apps/nextjs/pages/_app.tsx
+++ apps/nextjs/pages/_app.tsx
@@ -1,23 +1,23 @@
 // import '../styles/globals.css'
-import '../styles/global.scss';
-import '../styles/App.scss';
-import '@fontsource/finger-paint';
-import '@fontsource/roboto-mono';
-import '@fontsource/noto-sans';
-import '@fontsource/russo-one';
-import { withUrqlClient } from 'next-urql';
-import { cacheExchange, fetchExchange } from '@urql/core';
+import "../styles/global.scss";
+import "../styles/App.scss";
+import "@fontsource/finger-paint";
+import "@fontsource/roboto-mono";
+import "@fontsource/noto-sans";
+import "@fontsource/russo-one";
+import { withUrqlClient } from "next-urql";
+import { cacheExchange, fetchExchange } from "@urql/core";
 
-import { AuthProvider } from '../components/auth';
-import CookieConsent from 'react-cookie-consent';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
-import { gql, useQuery } from 'urql';
-import type { AppProps } from 'next/app';
+import { AuthProvider } from "../components/auth";
+import CookieConsent from "react-cookie-consent";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import { gql, useQuery } from "urql";
+import type { AppProps } from "next/app";
 
 const QUERY_EVENTS = gql`
 	query {
-		events {
+		events(where: { published: { equals: true } }, orderBy: { startDate: asc }) {
 			shortname
 			endDate
 			published
@@ -35,9 +35,8 @@ function AusSpeedrunsWebsite({ Component, pageProps }: AppProps) {
 			<Component {...pageProps} />
 			<Footer />
 			<CookieConsent
-				style={{ fontSize: '1.5rem' }}
-				buttonStyle={{ background: '#CC7722', color: '#FFFFFF', fontSize: '1.5rem' }}
-			>
+				style={{ fontSize: "1.5rem" }}
+				buttonStyle={{ background: "#CC7722", color: "#FFFFFF", fontSize: "1.5rem" }}>
 				This website uses cookies to function.
 			</CookieConsent>
 		</AuthProvider>
@@ -45,6 +44,6 @@ function AusSpeedrunsWebsite({ Component, pageProps }: AppProps) {
 }
 
 export default withUrqlClient((_ssrExchange) => ({
-	url: typeof window === undefined ? 'http://localhost:8000/api/graphql' : '/api/graphql',
+	url: typeof window === undefined ? "http://localhost:8000/api/graphql" : "/api/graphql",
 	exchanges: [cacheExchange, fetchExchange],
 }))(AusSpeedrunsWebsite);

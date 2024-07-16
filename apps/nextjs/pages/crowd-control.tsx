4203ed889c4ca19af91800e216659636fce5e740
Ewan Lyon <websites@ewanlyon.com>
2024-07-16 09:49:01 +0930

    fix crowd control

--- apps/nextjs/pages/crowd-control.tsx
+++ apps/nextjs/pages/crowd-control.tsx
@@ -18,9 +18,9 @@ export default function CrowdControl() {
 			</Head>
 			<div className={styles.background} />
 			<div className={`${styles.content} ${styles.form}`} style={{ padding: "5rem" }}>
-				<h1 style={{ marginBottom: "3rem" }}>Crowd Control at ASM2023</h1>
+				<h1 style={{ marginBottom: "3rem" }}>Crowd Control at ASM2024</h1>
 				<p>
-					During ASM2023, you will have the opportunity on four occasions to use{" "}
+					During ASM2024, you will have the opportunity on four occasions to use{" "}
 					<a target="_blank" rel="noopener noreferrer" href="https://crowdcontrol.live/">
 						Crowd Control
 					</a>
@@ -37,14 +37,14 @@ export default function CrowdControl() {
 				</p>
 				<h2>Runs</h2>
 				<ul>
-					<li>Hollow Knight: 12 July, 2:05 PM</li>
-					<li>Celeste: 14 July, 2:45 PM</li>
-					<li>The Elder Scrolls V: Skyrim: 15 July, 1:35 PM</li>
-					<li>Minecraft: 16 July, 3:40 PM</li>
+					<li>Hades: 17 July, 4:15 PM</li>
+					<li>Sonic Superstars: 19 July, 12:55 PM</li>
+					<li>Celeste: 20 July, 3:25 PM</li>
+					<li>Simpsons Hit and Run: 21 July, 1:30 PM</li>
 				</ul>
 				<h2>How to interact</h2>
 				<p>
-					The below is a short guide on how to interact with Crowd Control at ASM2023, being used by
+					The below is a short guide on how to interact with Crowd Control at ASM2024, being used by
 					AusSpeedruns in “Charity Mode”, meaning ALL Crowd Control donations go towards Game On Cancer, an
 					initiative of Cure Cancer, our charity of choice for this event.
 				</p>
@@ -55,18 +55,19 @@ export default function CrowdControl() {
 					</li>
 					<li>Go to “Donate”, then click “Donate to Cure Cancer”.</li>
 					<li>
-						This will link to the Tiltify campaign for ASM2023. Please note that donations made towards
-						ASM2023 <b>WITHOUT</b> having clicked on this link will not provide you with Crowd Control Coins.
-						<br/>Donations convert to Crowd Control Coins at a rate of 1c USD to 1 Coin.
+						This will link to the Tiltify campaign for ASM2024. Please note that donations made towards
+						ASM2024 <b>WITHOUT</b> having clicked on this link will not provide you with Crowd Control
+						Coins.
+						<br />
+						Donations convert to Crowd Control Coins at a rate of 1c USD to 1 Coin.
 					</li>
 					<li>
-						Fill out your desired donation amount,
-						email, Display Name for us to read out your donation (or anonymous if chosen), and then select
-						your payment method.
+						Fill out your desired donation amount, email, Display Name for us to read out your donation (or
+						anonymous if chosen), and then select your payment method.
 					</li>
 					<li>
-						Wait about 30 seconds from your donation going through, return to twitch, then click the coin icon to refresh your
-						balance; your coins are now ready to use!
+						Wait about 30 seconds from your donation going through, return to twitch, then click the coin
+						icon to refresh your balance; your coins are now ready to use!
 					</li>
 					<li>Click the “Effects” tab and have fun! </li>
 				</ol>
@@ -78,7 +79,7 @@ export default function CrowdControl() {
 					addressed to Crowd Control / Warp World.
 				</p>
 				<p>
-					AusSpeedruns wishes you all the best creating some Crowd Control chaos with our runners at ASM2023!
+					AusSpeedruns wishes you all the best creating some Crowd Control chaos with our runners at ASM2024!
 				</p>
 			</div>
 		</ThemeProvider>

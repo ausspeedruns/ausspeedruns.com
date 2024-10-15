ae4e4c795fb91741b27e3f3bcafd8e38afed3d8c
Ewan Lyon <websites@ewanlyon.com>
2024-10-15 20:25:50 +1100

    chore: update dependencies and remove mssql

--- apps/nextjs/pages/api/asmm/db.ts
+++
@@ -1,7 +0,0 @@
-import sql from 'mssql';
-
-const PASSWORD = process.env.ASMM_DATABASE_PASSWORD;
-
-export function connect(sqlLib: typeof sql) {
-	return sqlLib.connect(`Server=tcp:${process.env.ASMM_DATABASE_URL},1433;Initial Catalog=ASMM_DATA;Persist Security Info=False;User ID=asmmsqlaccess;Password=${PASSWORD};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;`)
-}

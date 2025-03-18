2d5f40d83e27fda89569073f7b7434f37f81fa17
Ewan Lyon <websites@ewanlyon.com>
2025-03-18 23:59:38 +1100

    add suspense

--- apps/nextjs/app/user/password-reset/page.tsx
+++ apps/nextjs/app/user/password-reset/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./User.PasswordReset.code.module.scss";
 import { Metadata } from "next";
 import { PasswordResetForm } from "./form";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
 	title: "Password Reset",
@@ -12,7 +13,9 @@ export default async function PasswordResetPage() {
 			<div className={styles.background}>what</div>
 			<div className={`${styles.content} ${styles.form}`}>
 				<h1>Password Reset</h1>
-				<PasswordResetForm />
+				<Suspense fallback={<div>Loading...</div>}>
+					<PasswordResetForm />
+				</Suspense>
 			</div>
 		</>
 	);
